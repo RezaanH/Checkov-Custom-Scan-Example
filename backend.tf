0fc9dc3a0d9915b69d9f48028bc4f5b64748a98c
RezH <rnhdigitalsolutions@gmail.com>
2025-11-09 19:17:42 +0000

    Simplifing Terraform

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "rezcloudterraformstate" # change this to your own bucket where you want your terraform backend
-    key     = "first-terraform-repo/terraform.tfstate" # set a path within the bucket, can leave it like this if you want.
-    region  = "eu-west-2" # chnange this to your prefered region 
-    encrypt = true
-  }
-}
--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 module "s3" {
   source      = "./modules/s3-bucket"
   bucket_name = "rez-first-terraform-module-s3bucket-v2" # change this to a unique name
